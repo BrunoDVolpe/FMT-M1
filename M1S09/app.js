40f62aa6ca283bc80bf3e145814f64f9f3475783
unknown <bruno_dv_alves@estudante.sesisenai.org.br>
2024-03-29 13:43:13 -0300

    Feat: ex2: /sobre and /contato

--- M1S09/app.js
+++ M1S09/app.js
@@ -2,4 +2,13 @@ const express = require("express")
 
 const app = express()
 
+// Exercício 2
+app.get("/sobre", (req, res) => {
+    res.send("Esse é meu exercício 2 usando node e express para a semana 9.")
+})
+
+app.get("/contato", (req, res) => {
+    res.json("Contato")
+})
+
 app.listen(3000)
