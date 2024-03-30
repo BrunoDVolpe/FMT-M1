d26820575473e520a74c870d294986798524a429
unknown <bruno_dv_alves@estudante.sesisenai.org.br>
2024-03-30 19:20:12 -0300

    Feat: ex6 - CRUD

---
+++ M1S09/crud/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const PORT = 3000
+
+// Lista de users simulando um banco de dados
+const users = [];
+
+// Adiciona um middleware para trabalhar com JSON nas reqs.
+app.use(express.json());
+
+// Criando um redirect na home para a /users
+app.get('/', (req, res) => {
+    res.redirect('/users')
+})
+
+// Exercício 6
+// Criar novo usuário
+app.post("/users", (req, res) => {
+    const user = req.body;
+    user.id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+    users.push(user)
+    res.status(201).json(user)
+})
+
+// Pegar todos os usuários
+app.get("/users", (req, res) => {
+    res.json({"users": users})
+})
+
+// Pegar dados de um usuário
+app.get("/users/:id", (req, res) => {
+    let { id } = req.params;
+    let user = users.find(user => user.id == parseInt(id))
+    if(!user) {
+        res.status(404).send("Usuário não encontrado.");
+        return
+    }
+    res.json(user)
+})
+
+// Atualizar dados de um usuário
+app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const index = users.findIndex(user => user.id == parseInt(id));
+    let newUserData = req.body;
+    if(index === -1){
+        res.status(404).send("Usuário não encontrado.")
+        return
+    }
+    users[index] = { ...users[index], ...newUserData }
+    users[index].id = id
+    res.status(200).json(users[index])
+})
+
+// Deletar usuário
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    let index = users.findIndex(user => user.id == parseInt(id))
+    if(index === -1){
+        res.status(404).send("Usuário não encontrado.")
+        return
+    }
+    users.splice(index, 1);
+    res.send("Usuário excluído com sucesso.")
+})
+
+app.listen(3000)
